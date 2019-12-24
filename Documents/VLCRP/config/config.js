46e0c35383cbd1724eca21f2ff6e857bef33e45f
Soarn <soarn@soarn.pro>
2019-12-24 13:31:28 -0800

    These are the files for vlcrp, they start VLC with discord rich presence straight from the terminal by doing 'VLCRP' (I think?)

---
+++ Documents/VLCRP/config/config.js
@@ -0,0 +1,25 @@
+function randomPass() {
+  return Math.random()
+    .toString(36)
+    .slice(-8);
+}
+
+module.exports = {
+  startupCommands: {
+    win32: 'C:/Program Files/VideoLAN/VLC/vlc.exe',
+    linux: '/usr/bin/vlc',
+    unix: '/usr/bin/vlc',
+    darwin: '/Applications/VLC.app/Contents/MacOS/VLC',
+  },
+  rpc: {
+    id: '410664151334256663',
+    updateInterval: 5000,
+    sleepTime: 30000,
+  },
+  vlc: {
+    password: 'vlc' || randomPass(), // Enter password between the semi-quotes
+    port: 8080,
+    address: 'localhost',
+    detached: false // Launch VLC seperately
+  },
+};
